2739596d092fe533dfba4f1d0fbad2d7b42afc26
MariosTsiro <barkasgreco@gmail.com>
2024-06-05 08:08:42 +0300

    Intro + menu

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GameController : MonoBehaviour
 {
@@ -13,9 +13,15 @@ public class GameController : MonoBehaviour
     public float waveWait;
 
     public Text scoreText;
+    public Text highScoreText; // New Text for displaying high score
     public Text restartText; 
     public Text gameOverText;  
+    public Button shopButton1; // Button for the first shop item
+    public Button shopButton2; // Button for the second shop item
+    public Button shopButton3; // Button for the third shop item
+
     private int score;
+    private int highScore; // New variable to store high score
     private bool gameOver;
     private bool restart;
 
@@ -28,7 +34,13 @@ public class GameController : MonoBehaviour
         gameOverText.text = "";
 
         score = 0;
-        UpdateScore ();
+        highScore = PlayerPrefs.GetInt("HighScore", 0); // Load high score from PlayerPrefs
+        UpdateScore();
+        UpdateHighScore(); // Update high score text
+
+        shopButton1.onClick.AddListener(BuyExtraLife);
+        shopButton2.onClick.AddListener(BuyDoubleSpeed);
+        shopButton3.onClick.AddListener(BuyTripleShoot);
 
         StartCoroutine (SpawnWaves ());
     }
@@ -71,6 +83,13 @@ public class GameController : MonoBehaviour
             {
                 restartText.text = "Press 'R' to Restart";
                 restart = true;
+                // Update high score if the current score is greater than the saved high score
+                if (score > highScore)
+                {
+                    highScore = score;
+                    PlayerPrefs.SetInt("HighScore", highScore); // Save high score to PlayerPrefs
+                    UpdateHighScore(); // Update high score text
+                }
                 break;
             }
         }
@@ -87,10 +106,46 @@ public class GameController : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    void UpdateHighScore()
+    {
+        highScoreText.text = "High Score: " + highScore;
+    }
+
     public void GameOver ()
     {
         gameOverText.text = "Game Over";
         gameOver = true;
     }
+
+    void BuyExtraLife()
+    {
+        if (score >= 100)
+        {
+            score -= 100;
+            // Add extra life functionality here
+            UpdateScore();
+        }
+    }
+
+    void BuyDoubleSpeed()
+    {
+        if (score >= 200)
+        {
+            score -= 200;
+            // Add double speed functionality here
+            UpdateScore();
+        }
+    }
+
+    void BuyTripleShoot()
+    {
+        if (score >= 300)
+        {
+            score -= 300;
+            // Add triple shoot functionality here
+            UpdateScore();
+        }
+    }
 }
+
 
